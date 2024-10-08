52da79b564199c7ce44913278cc55529de2f4312
fortune <160145307+fortunaratnadewi@users.noreply.github.com>
2024-10-08 23:01:02 +0700

    Perbaikan syarat password login regis

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -30,15 +30,15 @@
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+                <input type="email" class="form-control" id="email" name="email" required >
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+                <input type="password" class="form-control" id="password" name="password" required minlength="8">
             </div>
             <div class="mb-3">
                 <label for="password_confirmation" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required minlength="8">
             </div>
             <button type="submit" class="btn btn-primary">Register</button>
         </form>
